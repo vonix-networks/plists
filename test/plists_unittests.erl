387a4a70c17eab3bbc12b0d56d61606cc53ca7d4
Pierre Fenoll <pierrefenoll@gmail.com>
2017-07-14 13:55:03 -0700

    ready to publish on hex.pm (#1)
    * rebar3 + travis
    
    * fix travis
    
    * hex: upgrade tests
    
    * hex: disable tests for now...

--- test/plists_unittests.erl
+++ test/plists_unittests.erl
@@ -1,78 +1,71 @@
 -module(plists_unittests).
-%-export([run/0]).
--compile([export_all]).
+
+-include_lib("eunit/include/eunit.hrl").
 
 setup_network() ->
     Cookie = atom_to_list(erlang:get_cookie()),
     open_port({spawn, "erl -pa ../ebin -noshell -sname second_node@localhost -setcookie " ++ Cookie}, []),
-    wait_until_running().
+    wait_until_running(10).
 
-wait_until_running() ->
+wait_until_running(0) -> throw({error, could_not_connect});
+wait_until_running(N) ->
     case net_adm:ping(second_node@localhost) of
 	pong ->
 	    ok;
 	pang ->
 	    timer:sleep(20),
-	    wait_until_running()
+	    wait_until_running(N - 1)
     end.
 
 close_network() ->
     rpc:call(second_node@localhost, init, stop, []).
 
-run() ->
-    setup_network(),
-    try do_tests()
-    catch never -> never % junk so we can have after
-    after
-	    close_network()
-    end.
-
-do_tests() ->
-    do_tests(1),
-    do_tests(4),
-    do_tests({processes, 2}),
-    do_tests([4, {processes, 2}]),
-    do_tests({processes, schedulers}),
-    do_tests({timeout, 4000}),
-    do_tests({nodes, [{node(), 2}, node(), {node(), schedulers}]}),
-    do_tests({nodes, [{second_node@localhost, 2}, second_node@localhost,
-		      {second_node@localhost, schedulers}]}),
-    do_tests([{nodes, [{node(), 2}, second_node@localhost]}, {timeout, 4000}, 4]),
-    io:format("Ignore the ERROR REPORTs above, they are supposed to be there.~n"),
-    io:format("all tests passed :)~n").
+plists_test_() ->
+    {setup
+    ,fun setup_network/0
+    ,fun (_) -> close_network() end
+    ,fun (_) ->
+             do_tests(1)
+                 ++ do_tests(4)
+                 ++ do_tests({processes, 2})
+                 ++ do_tests([4, {processes, 2}])
+                 ++ do_tests({processes, schedulers})
+                 ++ do_tests({timeout, 4000})
+                 ++ do_tests({nodes, [{node(), 2}, node(), {node(), schedulers}]})
+                 ++ do_tests({nodes, [{second_node@localhost, 2}, second_node@localhost,
+                                      {second_node@localhost, schedulers}]})
+                 ++ do_tests([{nodes, [{node(), 2}, second_node@localhost]}, {timeout, 4000}, 4])
+             %% Ignore the ERROR REPORTs above, they are supposed to be there.
+     end
+    }.
 
 do_tests(Malt) ->
-    io:format("Testing with malt: ~p~n", [Malt]),
-    test_mapreduce(Malt),
-    test_all(Malt),
-    test_any(Malt),
-    test_filter(Malt),
-    test_fold(Malt),
-    test_foreach(Malt),
-    test_map(Malt),
-    test_partition(Malt),
-    test_sort(Malt),
-    test_usort(Malt),
-    check_leftovers(),
-    do_error_tests(Malt),
-    io:format("tests passed :)~n").
+    io:format(user, "Testing with malt: ~p~n", [Malt]),
+    test_mapreduce(Malt)
+        ++ test_all(Malt)
+        ++ test_any(Malt)
+        ++ test_filter(Malt)
+        ++ test_fold(Malt)
+        ++ test_foreach(Malt)
+        ++ test_map(Malt)
+        ++ test_partition(Malt)
+        ++ test_sort(Malt)
+        ++ test_usort(Malt)
+        ++ [?_assertEqual(nil, check_leftovers())]
+        ++ do_error_tests(Malt).
 
 do_error_tests(Malt) ->
-    {'EXIT', {badarith, _}} =
-	(catch plists:map(fun (X) -> 1/X end, [1,2,3,0,4,5,6], Malt)),
-    check_leftovers(),
-    if is_list(Malt) ->
-	    MaltList = Malt;
-       true ->
-	    MaltList = [Malt]
-    end,
-    MaltTimeout0 = [{timeout, 0}|MaltList],
-    {'EXIT', timeout} = (catch test_mapreduce(MaltTimeout0)),
-    check_leftovers(),
-    MaltTimeout40 = [{timeout, 40}|MaltList],
-    {'EXIT', timeout} = (catch plists:foreach(fun (_X) -> timer:sleep(1000) end, [1,2,3], MaltTimeout40)),
-    check_leftovers(),
-    'tests_passed :)'.
+    MaltList = case is_list(Malt) of
+                   true -> Malt;
+                   false -> [Malt]
+               end,
+    [?_assertError(badarith, plists:map(fun (X) -> 1/X end, [1,2,3,0,4,5,6], Malt))
+    ,?_assertEqual(nil, check_leftovers())
+    ,?_assertError(timeout, test_mapreduce([{timeout,0}|MaltList]))
+    ,?_assertEqual(nil, check_leftovers())
+    ,?_assertError(timeout, plists:foreach(fun (_X) -> timer:sleep(1000) end, [1,2,3], [{timeout,40}|MaltList]))
+    ,?_assertEqual(nil, check_leftovers())
+    ].
 
 check_leftovers() ->
     receive
@@ -114,10 +107,11 @@ test_mapreduce(Malt) ->
     3 = dict:fetch($e, TextAns2),
     2 = dict:fetch($h, TextAns2),
     1 = dict:fetch($m, TextAns2).
-    
+
 test_all(Malt) ->
-    true = plists:all(fun even/1, [2,4,6,8], Malt),
-    false = plists:all(fun even/1, [2,4,5,8], Malt).
+    [?_assert(plists:all(fun even/1, [2,4,6,8], Malt))
+    ,?_assert(not plists:all(fun even/1, [2,4,5,8], Malt))
+    ].
 
 even(X) when X rem 2 =:= 0 ->
     true;
@@ -125,55 +119,48 @@ even(_) ->
     false.
 
 test_any(Malt) ->
-    true = plists:any(fun even/1, [1,2,3,4,5], Malt),
-    false = plists:any(fun even/1, [1,3,5,7], Malt).
+    [?_assert(plists:any(fun even/1, [1,2,3,4,5], Malt))
+    ,?_assert(not plists:any(fun even/1, [1,3,5,7], Malt))
+    ].
 
 test_filter(Malt) ->
-    [2,4,6] = plists:filter(fun even/1, [1,2,3,4,5,6], Malt).
+    [?_assertEqual([2,4,6], plists:filter(fun even/1, [1,2,3,4,5,6], Malt))
+    ].
+
+some_folder(X, A) -> max(X * X, A).
+fuse(A1, A2) -> max(A1, A2).
 
 test_fold(Malt) ->
-    15 = plists:fold(fun (A, B) -> A+B end, 0, [1,2,3,4,5], Malt),
-    Fun = fun (X, A) ->
-		  X2 = X*X,
-		  if X2>A ->
-			  X2;
-		     true ->
-			  A
-		  end
-	  end,
-    Fuse = fun (A1, A2) when A1 > A2 ->
-		   A1;
-	       (_A1, A2) ->
-		   A2
-	   end,
     List = lists:seq(-5, 4),
-    25 = plists:fold(Fun, Fuse, -10000, List, Malt),
-    25 = plists:fold(Fun, {recursive, Fuse}, -10000, List, Malt).
+    [?_assertEqual(15, plists:fold(fun (A, B) -> A+B end, 0, [1,2,3,4,5], Malt))
+    ,?_assertEqual(25, plists:fold(fun some_folder/2, fun fuse/2, -10000, List, Malt))
+    ,?_assertEqual(25, plists:fold(fun some_folder/2, {recursive,fun fuse/2}, -10000, List, Malt))
+    ].
 
 test_foreach(_Malt) ->
-    whatever.
+    [].
 
 test_map(Malt) ->
-    [2,4,6,8,10] = plists:map(fun (X) -> 2*X end, [1,2,3,4,5], Malt),
-    % edge cases
-    [2] = plists:map(fun (X) -> 2*X end, [1], Malt),
-    [] = plists:map(fun (X) -> 2*X end, [], Malt).
+    [?_assertEqual([2,4,6,8,10], plists:map(fun (X) -> 2*X end, [1,2,3,4,5], Malt))
+     %% edge cases
+    ,?_assertEqual([2], plists:map(fun (X) -> 2*X end, [1], Malt))
+    ,?_assertEqual([], plists:map(fun (X) -> 2*X end, [], Malt))
+    ].
 
 test_partition(Malt) ->
-    {[2,4,6],[1,3,5]} = plists:partition(fun even/1, [1,2,3,4,5,6], Malt).
+    [?_assertEqual({[2,4,6],[1,3,5]}, plists:partition(fun even/1, [1,2,3,4,5,6], Malt))
+    ].
 
 test_sort(Malt) ->
-    Fun = fun (A, B) ->
-		  A =< B
-	  end,
-    [1,2,2,3,4,5,5] = plists:sort(Fun, [2,4,5,1,2,5,3], Malt),
-    % edge cases
-    [1] = plists:sort(Fun, [1], Malt),
-    [] = plists:sort(Fun, [], Malt).
+    Fun = fun erlang:'=<'/2,
+    [?_assertEqual([1,2,2,3,4,5,5], plists:sort(Fun, [2,4,5,1,2,5,3], Malt))
+     %% edge cases
+    ,?_assertEqual([1], plists:sort(Fun, [1], Malt))
+    ,?_assertEqual([], plists:sort(Fun, [], Malt))
+    ].
 
 test_usort(Malt) ->
-    Fun = fun (A, B) ->
-		  A =< B
-	  end,
-    [1,2,3,4,5] = plists:usort(Fun, [2,4,5,1,2,5,3], Malt),
-    [1,2,3,4,5] = plists:usort(Fun, [2,4,5,1,2,5,3], Malt).
+    Fun = fun erlang:'=<'/2,
+    [?_assertEqual([1,2,3,4,5], plists:usort(Fun, [2,4,5,1,2,5,3], Malt))
+    ,?_assertEqual([1,2,3,4,5], plists:usort(Fun, [2,4,5,1,2,5,3], Malt))
+    ].
